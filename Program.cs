26e12dad5869fb267fd1c6a3d794a07605715ec2
Gabriela <aragao.gabi@gmail.com>
2021-06-16 18:48:39 -0300

    Definição dos valores

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0 1 1 2 3 5 8 13 ...
+            
+            int a = 0,  b = 1, c = 0;
+
+            Console.Write("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequência de Fibonacci com  {0} valores", valores);
+
+            for(int i = 0; i <valores; i++)
+            {
+
+            }
+
+        }
+    }
+}
